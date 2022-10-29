1b441e957a02c9d858b1114fd6da5ba8de018c76
xingxing <xingxing@enai.im>
2022-10-29 22:49:57 +0800

    Add添加日志格式

--- mysql_test/orm_test.go
+++ mysql_test/orm_test.go
@@ -10,24 +10,24 @@ import (
     @date: 2022/10/29
 **/
 
-func Test(t *testing.T){
-	db,_ := sql.Open("splite3","gee.db")
-	defer func() {_=db.Close()}()
-	_,_ = db.Exec("DROP TABLE IF EXISTS User;")
-	_,_ = db.Exec("CREATE TABLE User(Name text);")
-	result,err :=db.Exec("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam")
-	if err!=nil{
-		affected,_ := result.RowsAffected()
+func Test(t *testing.T) {
+	db, _ := sql.Open("splite3", "gee.db")
+	defer func() { _ = db.Close() }()
+	_, _ = db.Exec("DROP TABLE IF EXISTS User;")
+	_, _ = db.Exec("CREATE TABLE User(Name text);")
+	result, err := db.Exec("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam")
+	if err != nil {
+		affected, _ := result.RowsAffected()
 		log.Println(affected)
 		row := db.QueryRow("SELECT Name FROM User LIMIT 1")
 		//rows,err :=db.Query("SELECT Name FROM User LIMIT 1")
 		//if err!=nil{
 		//	log.Println(err)
 		//}
-       //rows.Next()
+		//rows.Next()
 		var name string
-		if err := row.Scan(&name);err!=nil{
-		log.Println(name)
+		if err := row.Scan(&name); err != nil {
+			log.Println(name)
 		}
 	}
 }
